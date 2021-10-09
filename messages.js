d11cc392b367bf7f8c7030056f78ea9a92d7e050
C-Ingalls <92196779+C-Ingalls@users.noreply.github.com>
2021-10-09 17:22:47 -0500

    start outlining script

--- messages.js
+++ messages.js
@@ -4,5 +4,23 @@
     Use command line
     Develop locally on your computer
 
-    Topic for the messages: vicious mockery insults
+    Topic for the messages: vicious mockery insults 
 */
+
+//create 3 arrays to store the different phrases 
+// *** change array names *** maybe make an object to hold the arrays?
+const array1 = [];
+const array2 = [];
+const array3 = [];
+
+//this function generates the random message then returns it
+const generateMessage = () => {
+    let message = 'empty';
+    /*
+    write function code here
+    */
+    return message;
+}
+
+// log the message
+console.log(generateMessage());
